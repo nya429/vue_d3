16d02542207f9a99261a351fa2babdc6c5083e12
nya429 <song1991yue@gmail.com>
2022-02-01 01:54:15 -0500

    init

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="root">
+    <Canvas :csv-data='csv_data'
+    :variables='variableNames' />
+  </div>
+</template>
+
+<script>
+import Canvas from "./components/Canvas.vue";
+import * as d3 from "d3";
+import axios from "axios";
+
+export default {
+  name: "App",
+  components: {
+    Canvas
+  },
+  data() {
+    return {
+      variableNames: [
+        "name",
+        "mfr",
+        "type",
+        "calories",
+        "protein",
+        "fat",
+        "sodium",
+        "fiber",
+        "carbo",
+        "sugars",
+        "shelf",
+        "potass",
+        "vitamins",
+        "weight",
+        "cups"
+      ],
+      csv_data: []
+    };
+  },
+  methods: {
+    async loadData() {
+      const raw = await d3.text("cereal.csv");
+      const textString = raw
+        .split("\n")
+        .filter(s => !s.includes("-1"))
+        .join("\n");
+      const headers = this.variableNames.join("\t") + "\n";
+      return await d3.tsvParse(headers + textString.replaceAll(" ", "\t"));
+      // .map(d => {
+      //   delete d["shelf"];
+      //   return d;
+      // });
+    }
+  },
+  async beforeMount() {
+    this.csv_data = await this.loadData();
+    console.log("CSV loaded", this.csv_data);
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Canvas.vue
@@ -0,0 +1,183 @@
+<template>
+  <div class="canvas">
+    <h3>Data Visulization HW1 P1</h3>
+    <button>Click</button>
+    <svg id="canvas"></svg>
+  </div>
+</template>
+
+<script>
+import * as d3 from "d3";
+
+export default {
+  name: "Canvas",
+  data() {
+    return {
+      margin: {
+        top: 0,
+        right: 10,
+        bottom: 200,
+        left: 30
+      },
+      base: {
+        height: 600,
+        width: 1000
+      },
+      svg: null
+    };
+  },
+  props: {
+    csvData: Array,
+    variables: Array
+  },
+  watch: {
+    csvData(newVal, oldVal) {
+      // console.log('newVal', newVal.length, 'old', oldVal.length)
+      this.renderChart();
+    }
+  },
+  methods: {
+    renderChart() {
+      const variableNames = [
+        "calories",
+        "protein",
+        "fat",
+        "sodium",
+        "fiber",
+        "carbo",
+        "sugars",
+        "potass",
+        "vitamins"
+      ];
+
+      const numVariables = variableNames.length;
+      const numCases = this.csvData.length;
+      const xz = d3.range(numCases);
+
+      const yz = d3.range(numVariables).map(i => {
+        const key = variableNames[i];
+        return this.csvData.map(d => d[key]);
+      });
+
+      const y01z = d3
+        .stack()
+        .keys(d3.range(numVariables))(d3.transpose(yz)) // stacked yz
+        .map((data, i) => data.map(([y0, y1]) => [y0, y1, i]));
+
+      const yMax = d3.max(yz, y => d3.max(y));
+      const y1Max = d3.max(y01z, y => d3.max(y, d => d[1]));
+
+      const x = d3
+        .scaleBand()
+        .domain(xz)
+        .rangeRound([this.margin.left, this.base.width - this.margin.right])
+        .padding(0.08);
+
+      const y = d3
+        .scaleLinear()
+        .domain([0, y1Max])
+        .range([this.base.height - this.margin.bottom, this.margin.top]);
+
+      const z = d3
+        .scaleSequential(d3.interpolateBlues)
+        .domain([-0.5 * numVariables, 1.5 * numVariables]);
+
+      const xAxis = svg =>
+        svg
+          .append("g")
+          .attr(
+            "transform",
+            `translate(0, ${this.base.height - this.margin.bottom})`
+          )
+          .call(
+            d3
+              .axisBottom(x)
+              .tickSizeOuter(0)
+              .tickFormat(i => this.csvData[i]["name"])
+          )
+          .selectAll("text")
+          .style("text-anchor", "end")
+          .attr("dx", "-.8em")
+          .attr("dy", ".15em")
+          .attr("transform", "rotate(-65)");
+
+      const svg = d3.select("#canvas");
+      const rect = svg
+        .selectAll("g")
+        .data(y01z)
+        .enter()
+        .append("g")
+        .attr("fill", (d, i) => z(i))
+        .selectAll("rect")
+        .data(d => d)
+        .enter()
+        .append("rect")
+        .attr("x", (d, i) => x(i))
+        .attr("y", this.base.height - this.margin.bottom)
+        .attr("width", x.bandwidth())
+        .attr("height", 0);
+
+      svg.append("g").call(xAxis);
+
+      y.domain([0, y1Max]);
+
+      rect
+        .transition()
+        .duration(500)
+        .delay((d, i) => i * 20)
+        .attr("y", d => y(d[1]))
+        .attr("height", d => y(d[0]) - y(d[1]))
+        .transition()
+        .attr("x", (d, i) => x(i))
+        .attr("width", x.bandwidth());
+    },
+    renderBase() {
+      const svg = d3.select("#canvas");
+
+      svg
+        .attr("viewBox", [0, 0, this.base.width, this.base.height])
+        .attr("width", this.base.width)
+        .attr("height", this.base.height);
+      this.svg = svg;
+      // const rect = svg
+      //   .selectAll("g")
+      //   .data(this.csvData)
+      //   .join("g")
+      //   .attr("fill", (d, i) => z(i))
+      //   .selectAll("rect")
+      //   .data(d => d)
+      //   .join("rect")
+      //   .attr("x", (d, i) => x(i))
+      //   .attr("y", height - margin.bottom)
+      //   .attr("width", x.bandwidth())
+      //   .attr("height", 0);
+      // svg
+      //   .append("rect")
+      //   .attr("width", "100%")
+      //   .attr("height", "100%")
+      //   .attr("fill", "pink");
+    }
+  },
+  mounted() {
+    this.renderBase();
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
